b406174cd797ac29782c4d834ab5585861ccae3f
shinzzi <longnp1610@gmail.com>
2021-11-10 16:33:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { itemsTodo, itemsInProgress, itemsCompleted } from "./data";
+import { v4 } from "uuid";
+import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    todo: {
+      id: v4(),
+      title: "Todo",
+      items: itemsTodo,
+    },
+
+    "in-progress": {
+      id: v4(),
+      title: "In Progress",
+      items: itemsInProgress,
+    },
+
+    done: {
+      id: v4(),
+      title: "Complete",
+      items: itemsCompleted,
+    },
+  });
+
+  const onDragEnd = ({ destination, source }) => {
+    if (!destination) return;
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    )
+      return;
+
+    const itemm = Array.from(itemsTodo);
+    console.log(itemm.source);
+    console.log("from ", source);
+    console.log("to ", destination);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {/*  ['todo', 'in-progress', 'done']  */}
+        {Object.keys(state).map((data, index) => {
+          return (
+            // Return the column for each tag
+            <div key={index} className="column">
+              <h4>{state[data].title}</h4>
+              <Droppable droppableId={state[data].id}>
+                {(provided) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className="droppable-col"
+                    >
+                      {provided.placeholder}
+                      {/* Get values of item  */}
+                      {state[data].items.map((el, index) => {
+                        return (
+                          <Draggable
+                            key={el.id}
+                            index={index}
+                            draggableId={el.id}
+                          >
+                            {(provided) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.draggableProps}
+                                  {...provided.dragHandleProps}
+                                  className="droppable-item"
+                                >
+                                  {el.name}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import { v4 } from "uuid";
+
+export const itemsTodo = [
+  {
+    id: v4(),
+    name: "Clean the house",
+  },
+  {
+    id: v4(),
+    name: "Do homework",
+  },
+  {
+    id: v4(),
+    name: "Washing dishes",
+  },
+];
+
+export const itemsInProgress = [];
+export const itemsCompleted = [];
